dac7f5a248ec49b5132b0a1122d90c1b30a4848a
Micke <micke.welander@live.se>
2023-05-05 20:26:24 +0200

    Added a second screen xml and java file. Added a button that switches to the second screen

---
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -0,0 +1,16 @@
+package com.example.project;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+    }
+
+}
